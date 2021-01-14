53b47662d3712ebe4f0ff4fab3e33c24c7057bf7
asuman aydın <asuman.aydin6709@gmail.com>
2021-01-14 22:20:52 +0300

    new ids and general mockup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 import { PokemonProvider } from './PokemonContext';
-import PokemonList from './PokemonList';
+import PokemonsList from './PokemonList';
 import Pokedex from './Pokedex';
+import PokemonForm from './pokemonForm';
 
 const App = () => (
   <PokemonProvider>
     <div className="main">
-      <PokemonList />
+      <PokemonsList />
       <Pokedex />
     </div>
-    
+    <div className='form-wrapper'>
+      <PokemonForm />
+    </div>
   </PokemonProvider>
 );
 
